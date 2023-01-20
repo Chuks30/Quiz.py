68d1f7f8e1df6d0796eab143c5a49789029bf8f2
Chuks30 <121009976+Chuks30@users.noreply.github.com>
2023-01-20 08:13:03 +0100

    Add files via upload

---
+++ Inches.py
@@ -0,0 +1,7 @@
+import math
+
+inches_str = input("How many inches of rain have fallen: ")
+inches_int = int(inches str)
+volume = (inches int/12)*43560
+gallons = volume * 7.48051945
+print(inches int," in. rain on 1 acre is", gallons, "gallons")
---
+++ Quiz.py
@@ -0,0 +1,14 @@
+# Welcom to your favorite quiz
+# Promt user to enter name
+# Answer the following questions
+
+print("Welcome to your favorite quiz")
+print("Answer the following Questions")
+input_str = input("Enter your name:")
+input_str = input("What is the squear of 10:")
+if input_str == "100":
+	print("correct")
+elif input_str == "0-99":
+	print("wrong")
+print("the square of 10 is: ",10 ** 10)
+print("Would you like to continue")
